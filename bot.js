448b15b0336896fd1e6f1576d26a92e79ab9d012
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-08-31 15:05:17 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "+"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('ready', () => {
+    client.user.setActivity("hi",{type: 'WATCHING'})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
