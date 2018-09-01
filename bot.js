a4a4b541c0603b4101b629da442d2d6d55973a02
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-01 09:11:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -412,4 +412,30 @@ client.on('message' , message => {
   }  
  });
 
+
+client.on('message', message =>{
+    let args = message.content.split(' ');
+    let prefix = '!'; //تقدر تغير البرفكس
+    
+    if(args[0] === `${prefix}avatar`){
+        let mentions = message.mentions.members.first()
+        if(!mentions) {
+          let sicon = message.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(message.author.avatarURL)
+          .setColor("#f7abab") 
+          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
+          message.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#f7abab")
+          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
+          .setImage(sicon)
+          message.channel.send({embed})
+        }
+    };
+});
+
+
 client.login(process.env.BOT_TOKEN);
