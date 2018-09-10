8d4bf6a4e73b36697523d64bb7809e4299732067
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-11 01:51:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,161 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Abdo1#4651`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+client.on('message', message => {
+var prefix = "$";
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'all') {
+    if (!args[1]) {
+message.channel.send("**bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
+
+
+client.on('message', message => {
+  if (true) {
+if (message.content === '$بوت') {
+      message.author.send('  https://discordapp.com/api/oauth2/authorize?client_id=488850031571369995&permissions=8&scope=bot  |  اتفضل رابط البوت     ').catch(e => console.log(e.stack));
+ 
+    }
+   }
+  });
+ 
+ 
+client.on('message', message => {
+     if (message.content === "+بوت") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+client.on('message',function(message) {
+  if (message.author.bot) return;
+
+
+                  if(!message.channel.guild) return;
+
+                    if (message.content === prefix + "members") {
+ const embed = new Discord.RichEmbed()
+
+    .setDescription(`**Members info ✨
+ اونلاين:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+  حاله حمراء       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+  حاله صفراء:     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
+  الاوفلاين:     ${message.guild.members.filter(m=>m.presence.status == 'offline').size}
+   عدد الاعضاء:  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size}
+ البوتات: ${message.guild.members.filter(m=>m.user.bot).size} **`)
+         message.channel.send({embed});
+
+    }
+      });
+
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help") {
+		 message.channel.send('**The Message Was Sent On Private**');
+            
+	
+		 
+
+
+ message.author.sendMessage(`
+ **
+ 
+
+
+╔[❖════════════❖]╗
+             Prefix = ' + '
+╚[❖════════════❖]╝
+
+╔[❖════════════❖]╗
+            الاوامر العامة
+╚[❖════════════❖]╝
+
+❖ +bc ➾ رسالة جماعية
+
+❖ +members ➾ معلومات الاعضاء
+
+❖ +ping ➾ لمعرفة سرعة الاتصال
+
+❖ +avatar ➾ لعرض صورة الحساب
+
+❖ +id ➾ معلومات حسابك
+
+╔[❖════════════❖]╗
+            اوامر الاغاني
+╚[❖════════════❖]╝
+
+❖ !play ➾ لتشغيل الاغنية 
+
+❖ !stop ➾ لإيقاف الاغنية
+
+==================================================================
+
+
+`);
+
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
